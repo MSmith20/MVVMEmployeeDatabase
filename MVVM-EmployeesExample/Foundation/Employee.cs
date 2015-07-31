e5ef8aa57d2dbd0cbd2049a320e11adf05ce3b3d
unknown <Michael.Smith2@DWSRW02.exprogroup.local>
2015-07-31 12:33:26 +0100

    Revert "no message"
    This reverts commit 5944fedf10d701c12c3a863531334fd1ea2d5104.

--- MVVM-EmployeesExample/Foundation/Employee.cs
+++ MVVM-EmployeesExample/Foundation/Employee.cs
@@ -9,10 +9,8 @@ namespace MVVM_EmployeesExample
     class Employee
     {
         public string Name { get; set; }
-        public string Role { get; set; }
-        public string Address { get; set; }
         public int Number { get; set; }
         public int Years { get; set; }
-        public int EmergencyNumber { get; set; }
+        public string Title { get; set; }
     }
 }
